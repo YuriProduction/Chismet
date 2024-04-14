da2637c904093cd6892cda272be17dfca55c133f
Jurex <jurexda@yandex.ru>
2024-04-15 00:11:07 +0500

    Done

---
+++ University/Chismet/Laba4Chismet.py
@@ -0,0 +1,47 @@
+import math
+from sympy import diff
+
+
+def main():
+	h_i = [0.1, 0.05, 0.025]
+	left_rectangles(h_i, 0, 2)
+	print()
+	euler(h_i, 0, 2)
+
+
+def func(x):
+	return math.sin(math.sqrt(1 + x * x))
+
+
+def derivative(x):
+	return diff(func(x))
+
+
+def left_rectangles(h_i: list, a, b):
+	for h in h_i:
+		x = a
+		integral_val = 0
+		m = int((b - a) / h)
+		for i in range(m):
+			integral_val += (h * func(x))
+			x += h
+		print(f"for h = {h}, m = {m}, integral value is {integral_val}")
+
+
+def euler(h_i, a, b):
+	for h in h_i:
+		m = int((b - a) / h)
+		x = a
+		integral_val = 0
+		integral_val += 0.5 * func(a)
+		x += h
+		for i in range(1, m):
+			integral_val += func(x)
+			x += h
+		integral_val += 0.5 * func(b)
+		integral_val *= h
+		integral_val += (1 / 12) * h * h * (derivative(a) - derivative(b))
+		print(f"for h = {h}, m = {m}, integral value is {integral_val}")
+
+
+main()
